e02bf36c4d59880c7fbd82ca5af25dd0a3ee0df4
Роман Савощик <tweakice@gmail.com>
2022-08-03 18:57:52 +0600

    Add form for handle keyboards

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
 import PrimaryButton from "../components/primary-button";
+import abi from "../utils/Keyboards.json";
+import { ethers } from "ethers";
 
 export default function Home() {
 
   const [ethereum, setEthereum] = useState(undefined);
   const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([]);
+  const [newKeyboard, setNewKeyboard] = useState("")
+
+  const contractAddress = "0xbAd827EA6fad693782707E1fF631A6c7BfD19eeb";
+  const contractABI = abi.abi;
 
   const handleAccounts = (accounts) => {
     if (accounts.length > 0) {
       const account = accounts[0];
-      console.log("We have an authorized account: ", account); 
+      console.log("We have an authorized account: ", account);
+      setConnectedAccount(account); 
     } else {
       console.log("No authorized accounts yet");
     }
@@ -25,7 +33,20 @@ export default function Home() {
       handleAccounts(accounts);
     }
   };
-  useEffect(() => getConnectedAccount());
+  useEffect(() => getConnectedAccount(), []);
+
+  const getKeyboards = async () => {
+    if (ethereum && connectAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      const keyboards = await keyboardsContract.getKeyboards();
+      console.log('Retrieved keyboards...', keyboards)
+      setKeyboards(keyboards);
+    }
+  };
+  useEffect(() => getKeyboards(), [connectedAccount])
 
   const connectAccount = async () => {
     if (!ethereum) {
@@ -44,6 +65,47 @@ export default function Home() {
   if (!connectedAccount) {
     return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
   }
+
+  const submitCreate = async (e) => {
+    e.preventDefault();
+
+    if (!ethereum) {
+      console.error("Ethereum object is required to create a keyboard");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log("Create a transaction started... ", createTxn.hash);
+
+    await createTxn.wait();
+    console.log("Created keyboard!", createTxn.hash);
+
+    await getKeyboards();
+  }
     
-  return <p>Connected Account: {connectedAccount}</p>
+  return (
+    <div className="flex flex-col gap-y-8">
+      <form className="flex flex-col gap-y-2">
+        <div>
+          <label htmlFor="keyboard-description" className="block text-sm font-medium text-gray-700">
+            Keyboard Description
+          </label>
+        </div>
+        <input
+          name="keyboard-type"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          value={newKeyboard}
+          onChange={(e) => { setNewKeyboard(e.target.value) }}
+        />
+        <PrimaryButton type="submit" onClick={submitCreate}>
+          Create Keyboard!
+        </PrimaryButton>
+      </form>
+      <div>{keyboards.map((keyboard, i) => <p key={i}>{keyboard}</p>)}</div>
+    </div>
+  )
 }
