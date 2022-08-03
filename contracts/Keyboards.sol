ee8f2ceec7ace953c4ccd25c7b7095f67044d1f9
Роман Савощик <tweakice@gmail.com>
2022-08-03 21:51:14 +0600

    Add enum and struct to base contract

--- contracts/Keyboards.sol
+++ contracts/Keyboards.sol
@@ -2,7 +2,22 @@
 pragma solidity ^0.8.15;
 
 contract Keyboards {
-  string[] public createdKeyboards;
+  enum KeyboardKind { 
+	  SixtyPercent, 
+	  SeventyFivePercent,
+    EightyPercent, 
+	  Iso105 
+	}
+
+  struct Keyboard {
+    KeyboardKind kind; 
+    // ABS = false, PBT = true
+    bool isPBT;
+    // tailwind filters to layer over
+    string filter;
+  }
+
+  Keyboard[] public createdKeyboards;
 
   function getKeyboards() view public returns(string[] memory) {
     return createdKeyboards;
