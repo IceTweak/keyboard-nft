a7080b6357141a0ac601251a7990e1b1bee6490f
Роман Савощик <tweakice@gmail.com>
2022-08-05 19:31:03 +0600

    Add account provider component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast'
+import MetaMaskAccountProvider from '../components/meta-mask-account-provider';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Toaster />
+    <MetaMaskAccountProvider>
+      <Toaster 
+      position='top-right'
+      reverseOrder={false}
+      />
       <Component {...pageProps} />
-    </>
+    </MetaMaskAccountProvider>
   )
 }
 export default MyApp
